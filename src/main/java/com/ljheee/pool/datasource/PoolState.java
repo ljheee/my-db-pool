19891f48f81fe70529473b6b1657673504364fab
lijianhua04 <lijianhua04@meituan.com>
2018-10-20 11:48:16 +0800

    mv

--- src/main/java/com/ljheee/pool/datasource/PoolState.java
+++ src/main/java/com/ljheee/pool/datasource/PoolState.java
@@ -4,12 +4,11 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- * 维护 活跃连接队列 和 空闲队列
+ * 维护 活跃队列 和 空闲队列
  */
 public class PoolState {
 
     protected final List<PooledConnection> idleConnections = new ArrayList();
     protected final List<PooledConnection> activeConnections = new ArrayList();
-
 
 }
